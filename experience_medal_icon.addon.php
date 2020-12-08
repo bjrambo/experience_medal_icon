b0fdd7686ea1bb7a059a43b4b47da0687a446552
BJRambo <qw5414@naver.com>
2020-12-08 13:54:47 +0900

    Change to dir

--- experience_medal_icon.addon.php
+++ experience_medal_icon.addon.php
@@ -12,7 +12,7 @@
 		return;
 	}
 
-	require_once(_XE_PATH_ . 'addons/experience_level_icon/experience_level_icon.lib.php');
+	require_once(_XE_PATH_ . 'addons/experience_medal_icon/experience_medal_icon.lib.php');
 	
 	$temp_output = preg_replace_callback('!<(div|span|a)([^\>]*)member_([0-9\-]+)([^\>]*)>(.*?)\<\/(div|span|a)\>!is', 'experienceLevelIconTrans', $output);
 	if($temp_output)
